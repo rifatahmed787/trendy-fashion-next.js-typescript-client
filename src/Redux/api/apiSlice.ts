a2b7e74322d4afa45a92479bcc6d77c430f7582f
Md Rifat <mdrifatahmed787@gmail.com>
2023-12-15 16:40:02 +0600

    build project

--- src/Redux/api/apiSlice.ts
+++ src/Redux/api/apiSlice.ts
@@ -6,6 +6,7 @@ export const apiSlice = createApi({
   reducerPath: "apis",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://trendy-fashion-server.vercel.app/api/v1",
+
     // baseUrl: "http://localhost:5000/api/v1",
 
     prepareHeaders(headers, { getState }) {
